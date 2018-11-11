fdf0f4cd3490c5ef235b869a8c253abaf264a91d
夏为 <2896916771@qq.com>
2018-11-11 18:39:37 +0800

    夏为提交

--- src/wugong/huashanjianfawugongdaquan.java
+++ src/wugong/huashanjianfawugongdaquan.java
@@ -5,6 +5,14 @@ public class huashanjianfawugongdaquan {
 	public static void main(String[] args) {
 		System.out.println("吸星大法");
 		System.out.println("九阳神功");
+		System.out.println("九阳神功");
+		System.out.println("九阳神功");
+		System.out.println("九阳神功");
+		System.out.println("九阳神功");
+		System.out.println("九阳神功");
+		System.out.println("九阳神功");
+		System.out.println("九阳神功");
+		System.out.println("九阳神功");
 
 	}
 
